305f7debec3ff0797194422efe02e03ce0c901c1
ali <aliahmadinasab81@gmail.com>
2023-02-17 16:29:59 +0330

    Update Browser_Actions_Session_9.py

--- Browser_Actions_Session_9.py
+++ Browser_Actions_Session_9.py
@@ -1,1 +1,16 @@
-a
+"""
+Session 9 - Mouse Actions
+    * Click
+    * Double Click
+    * Right Click
+    * Move Cursor
+    * Click and Hold
+    * Release
+    * Drag and Drop 1
+    * Drag and Drop 2
+    * Offset of element (Coordinates)
+    * Move by Offset
+    * Drag and  Drop by Offset
+    * Element Rect
+"""
+
