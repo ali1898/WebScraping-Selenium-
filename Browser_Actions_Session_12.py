03647b0091ba8911e9e3b7e236a8c9d72ba496c9
ali <aliahmadinasab81@gmail.com>
2023-02-24 15:17:50 +0330

    Selenium

--- Browser_Actions_Session_12.py
+++ Browser_Actions_Session_12.py
@@ -43,62 +43,98 @@ driver.implicitly_wait(1)
 #     print(datetime.now())
 
 
-# Wait until element has an attribute
-def wait_until_element_has_an_attribute(element_selector,
-                                        element_locator,
-                                        attribute,
-                                        attribute_value,
-                                        timeout=5,
-                                        exact=True
-                                        ):
-    for i in range(timeout * 5):
-        try:
-            element = driver.find_element(element_selector, element_locator)
-            if exact:
-                assert element.get_attribute(attribute) == attribute_value
-            else:
-                assert attribute_value in element.get_attribute(attribute)
-            print(" Element attribute met: " + str(attribute_value))
-            return
-        except:
-            sleep(0.2)
-    raise Exception("Element attribute is not: " + str(attribute_value))
-
+# # Wait until element has an attribute
+# def wait_until_element_has_an_attribute(element_selector,
+#                                         element_locator,
+#                                         attribute,
+#                                         attribute_value,
+#                                         timeout=5,
+#                                         exact=True
+#                                         ):
+#     for i in range(timeout * 5):
+#         try:
+#             element = driver.find_element(element_selector, element_locator)
+#             if exact:
+#                 assert element.get_attribute(attribute) == attribute_value
+#             else:
+#                 assert attribute_value in element.get_attribute(attribute)
+#             print(" Element attribute met: " + str(attribute_value))
+#             return
+#         except:
+#             sleep(0.2)
+#     raise Exception("Element attribute is not: " + str(attribute_value))
+#
+# # driver.get("https://play1.automationcamp.ir/expected_conditions.html")
+# # trigger = driver.find_element(By.ID, "enabled_trigger")
+# # trigger.location_once_scrolled_into_view
+# # trigger.click()
+# # wait_until_element_has_an_attribute(By.ID, "enabled_target", 'class', 'success', exact=False)
+#
+#
+# # Wait until element has not an attribute
+# def wait_until_element_has_not_an_attribute(element_selector,
+#                                         element_locator,
+#                                         attribute,
+#                                         attribute_value,
+#                                         timeout=5,
+#                                         exact=True
+#                                         ):
+#     for i in range(timeout * 5):
+#         try:
+#             element = driver.find_element(element_selector, element_locator)
+#             if exact:
+#                 assert element.get_attribute(attribute) != attribute_value
+#             else:
+#                 assert attribute_value not in element.get_attribute(attribute)
+#             print(" Element attribute not in or not equals: " + str(attribute_value))
+#             return
+#         except:
+#             sleep(0.2)
+#     raise Exception("Element attribute in or equals: " + str(attribute_value))
+#
+#
 # driver.get("https://play1.automationcamp.ir/expected_conditions.html")
 # trigger = driver.find_element(By.ID, "enabled_trigger")
 # trigger.location_once_scrolled_into_view
+# wait_until_element_has_an_attribute(By.ID, "enabled_target", 'class', 'danger', exact=False)
+# wait_until_element_has_not_an_attribute(By.ID, "enabled_target", 'class', 'success', exact=False)
 # trigger.click()
 # wait_until_element_has_an_attribute(By.ID, "enabled_target", 'class', 'success', exact=False)
+# wait_until_element_has_not_an_attribute(By.ID, "enabled_target", 'class', 'danger', exact=False)
+# print("Test case PASSED")
 
 
-# Wait until element has not an attribute
-def wait_until_element_has_not_an_attribute(element_selector,
-                                        element_locator,
-                                        attribute,
-                                        attribute_value,
-                                        timeout=5,
-                                        exact=True
-                                        ):
-    for i in range(timeout * 5):
+# Wait until elements is disable/enable
+def wait_until_elements_is_enable(element_selector,
+                                 element_locator,
+                                 timeout
+                                  ):
+    for i in range(timeout * 2):
         try:
             element = driver.find_element(element_selector, element_locator)
-            if exact:
-                assert element.get_attribute(attribute) != attribute_value
-            else:
-                assert attribute_value not in element.get_attribute(attribute)
-            print(" Element attribute not in or not equals: " + str(attribute_value))
+            assert element.is_enabled()
             return
         except:
-            sleep(0.2)
-    raise Exception("Element attribute in or equals: " + str(attribute_value))
+            sleep(0.5)
 
+
+def wait_until_elements_is_disable(element_selector,
+                                   element_locator,
+                                   timeout
+                                   ):
+    for i in range(timeout * 2):
+        try:
+            element = driver.find_element(element_selector, element_locator)
+            assert not element.is_enabled()
+            return
+        except:
+            sleep(0.5)
 
 driver.get("https://play1.automationcamp.ir/expected_conditions.html")
 trigger = driver.find_element(By.ID, "enabled_trigger")
 trigger.location_once_scrolled_into_view
-wait_until_element_has_an_attribute(By.ID, "enabled_target", 'class', 'danger', exact=False)
-wait_until_element_has_not_an_attribute(By.ID, "enabled_target", 'class', 'success', exact=False)
+wait_until_elements_is_disable(By.ID, "enabled_target", 5)
+print("Element is Desable now!")
 trigger.click()
-wait_until_element_has_an_attribute(By.ID, "enabled_target", 'class', 'success', exact=False)
-wait_until_element_has_not_an_attribute(By.ID, "enabled_target", 'class', 'danger', exact=False)
-print("Test case PASSED")
+wait_until_elements_is_enable(By.ID, "enabled_target", 5)
+print("Element is enabled now!")
